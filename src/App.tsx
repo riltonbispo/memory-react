c7339cf39b00d09f6aca023bb6c53784707090a0
riltonbispo <riltonbispo1@gmail.com>
2023-02-15 17:24:38 -0300

    feat: adicionando logica para preencher grid

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,15 @@ const App = () => {
     }
 
     // preencher grid
-    setGridItems([]);
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
 
     // passsar o grid temporario para o state
     setGridItems(tmpGrid);
