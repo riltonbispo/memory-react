e87b1b65383304f89769daf0fce1caf8b91d5872
riltonbispo <riltonbispo1@gmail.com>
2023-02-15 16:31:13 -0300

    feat: adicionando type para os itens do grid
    - adicioando os state que serão utilizados futuramente

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -50,4 +50,11 @@ export const GridArea = styled.div`
   }
 `
 
+export const Grid = styled.div`
+  width: 430px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
+
+
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
-import RestartIcon from './svgs/restart.svg'
-import { Button } from "./components/Button";
+import RestartIcon from "./svgs/restart.svg";
 
+import { Button } from "./components/Button";
 import { InfoItem } from "./components/InfoItem";
+import { GridItemType } from "./types/GridItemTypes";
 
 const App = () => {
- 
-  const resetAndCreatGrid = () =>{
+  const [playing, setPlaying] = useState<boolean>(false)
+  const [timeElapsed, setTimeElapsed] = useState<number>(0)
+  const [moveCount, setMoveCount] = useState<number>(0)
+  const [shownCount, setShownCount] = useState<number>(0)
+  const [gridItems, setGridItems] = useState<GridItemType[]>([])
 
-  }
+
+  useEffect(() => resetAndCreatGrid(), []);
+
+  const resetAndCreatGrid = () => {
+
+
+  };
 
   return (
     <C.Container>
@@ -23,9 +36,15 @@ const App = () => {
           <InfoItem label="Movimentos" value="0"></InfoItem>
         </C.InfoArea>
 
-        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreatGrid} ></Button>
+        <Button
+          label="Reiniciar"
+          icon={RestartIcon}
+          onClick={resetAndCreatGrid}
+        ></Button>
       </C.Info>
-      <C.GridArea>...</C.GridArea>
+      <C.GridArea>
+        <C.Grid></C.Grid>
+      </C.GridArea>
     </C.Container>
   );
 };
