83ba334cc37150a749a7f58e96070d9fe10936bd
riltonbispo <riltonbispo1@gmail.com>
2023-02-15 17:04:55 -0300

    feat: criação de logica grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
 import RestartIcon from "./svgs/restart.svg";
@@ -8,20 +7,41 @@ import RestartIcon from "./svgs/restart.svg";
 import { Button } from "./components/Button";
 import { InfoItem } from "./components/InfoItem";
 import { GridItemType } from "./types/GridItemTypes";
+import { items } from "./data/items";
 
 const App = () => {
-  const [playing, setPlaying] = useState<boolean>(false)
-  const [timeElapsed, setTimeElapsed] = useState<number>(0)
-  const [moveCount, setMoveCount] = useState<number>(0)
-  const [shownCount, setShownCount] = useState<number>(0)
-  const [gridItems, setGridItems] = useState<GridItemType[]>([])
-
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
   useEffect(() => resetAndCreatGrid(), []);
 
   const resetAndCreatGrid = () => {
+    // resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
 
+    // criação de grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < items.length * 2; i++) {
+      tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false,
+      });
+    }
 
+    // preencher grid
+    setGridItems([]);
+
+    // passsar o grid temporario para o state
+    setGridItems(tmpGrid);
+
+    // começar o jogo
+    setPlaying(true);
   };
 
   return (
